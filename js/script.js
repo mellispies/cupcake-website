624ea458531ed1ecddd6d41821ff65b467516164
mellispies <44965502+mellispies@users.noreply.github.com>
2019-09-06 22:35:05 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	
+
+//magnific popup
+$('.parent-container').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image',
+
+  gallery: {
+  	enabled: true
+  }
+  // other options
+});
+
+});
