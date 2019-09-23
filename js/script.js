f103eaa167fb2be238c3adfc4ad03e031d29f7d3
mellispies <44965502+mellispies@users.noreply.github.com>
2019-09-23 22:39:52 +0200

    added sticky navbar & animation on navbar toggler

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
 	
+//toggler button
+$('.navbar-toggler').click(function(){
+
+	$('.navbar-toggler').toggleClass('change')
+
+})
+
+
+//sticky navbar & less padding 
+$(window).scroll(function () {
+
+	let position = $(this).scrollTop();
+
+	if(position >= 633) {
+		$('.navbar').addClass('navbar-background');
+		$('.navbar').addClass('fixed-top');
+	} else {
+		$('.navbar').removeClass('navbar-background');
+		$('.navbar').removeClass('fixed-top');
+	}
+})
+
+
+
+
+
 
 //magnific popup
 $('.parent-container').magnificPopup({
